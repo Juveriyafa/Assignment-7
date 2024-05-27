26f1e814d880c9a74464d85c4ed89ea043d9c18f
Juveriyafa <jfarooqui2003@gmail.com>
2024-05-27 21:10:01 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,9 @@
+let multiplesOfThree = [];
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0) {
+        multiplesOfThree.push(i);
+    }
+}
+
+console.log(multiplesOfThree);
---
+++ 2.js
@@ -0,0 +1,8 @@
+function printNumbers() {
+    for (let i = 0; i <= 100; i++) {
+        console.log(i);
+    }
+}
+
+// Call the function to print the numbers
+printNumbers();
